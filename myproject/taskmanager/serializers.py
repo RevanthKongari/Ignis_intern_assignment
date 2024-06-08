c80244f0efa6c34795f599e1a2182f48e15acf06
RevanthKongari <146952582+RevanthKongari@users.noreply.github.com>
2024-06-08 22:26:32 +0530

    Delete myproject directory

--- myproject/taskmanager/serializers.py
+++
@@ -1,7 +0,0 @@
-
-from rest_framework import serializers
-
-class StartScrapingSerializer(serializers.Serializer):
-    coins = serializers.ListField(
-        child=serializers.CharField(max_length=10)
-    )
--- myproject/taskmanager/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from .scraper import CoinMarketCapScraper
-
-@shared_task
-def scrape_data(coin_acronyms):
-    scraper = CoinMarketCapScraper()
-    return scraper.scrape(coin_acronyms)
